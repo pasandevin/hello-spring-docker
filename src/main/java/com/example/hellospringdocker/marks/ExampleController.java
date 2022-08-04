dd4e310f62b909363670a06107b52b7a0613fcff
pasandevin <jayawardene.pasandevin@gmail.com>
2022-08-05 00:36:51 +0530

    established db connection

--- src/main/java/com/example/hellospringdocker/marks/ExampleController.java
+++ src/main/java/com/example/hellospringdocker/marks/ExampleController.java
@@ -1,7 +1,9 @@
 package com.example.hellospringdocker.marks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "mark")
 public class ExampleController {
 
-    @GetMapping("getmarks")
-    public Mark getMarksForUser() {
+    MarkRepository markRepository;
+
+    @Autowired
+    public ExampleController(MarkRepository markRepository) {
+        this.markRepository = markRepository;
+    }
+
+    @GetMapping("putmarks")
+    public void getMarksForUser() {
         Mark mark = new Mark("Krishan", 80);
-        return  mark;
-//        return "hello";
+        markRepository.save(mark);
     }
 }
--- src/main/java/com/example/hellospringdocker/marks/Mark.java
+++ src/main/java/com/example/hellospringdocker/marks/Mark.java
@@ -1,11 +1,25 @@
 package com.example.hellospringdocker.marks;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Mark {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public int id;
     public String name;
     public int marks;
 
     public Mark(String name, int marks) {
         this.name = name;
         this.marks = marks;
+    }
+
+    public Mark() {
+
     }
 }
